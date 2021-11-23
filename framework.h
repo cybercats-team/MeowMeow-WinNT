455d1ddde1d337204aa9a6bced0a33039a458c22
Cyberneticist <Cyberneticist@CYBERNETICIST>
2021-11-24 01:21:27 +0200

    Add project files.

---
+++ framework.h
@@ -0,0 +1,15 @@
+// header.h : include file for standard system include files,
+// or project specific include files
+//
+
+#pragma once
+
+#include "targetver.h"
+
+// Exclude rarely-used stuff from Windows headers
+#define WIN32_LEAN_AND_MEAN             
+// Windows Header Files
+#include <windows.h>
+
+// C/C++ Standard Library
+#include <stdlib.h>
